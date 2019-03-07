c3cfeac5d6ce21e28ce42d1b7d4d9941ba41897f
iyinraphael <iyinraphael@gmail.com>
2019-03-07 14:27:27 -0800

    storyboard created and outlets connected

--- RPN-Calculator-ObjC/View Controller/RPNCalculatorViewController.h
+++ RPN-Calculator-ObjC/View Controller/RPNCalculatorViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  RPNCalculatorViewController.h
 //  RPN-Calculator-ObjC
 //
 //  Created by Iyin Raphael on 3/7/19.
@@ -8,8 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+NS_ASSUME_NONNULL_BEGIN
 
+@interface RPNCalculatorViewController : UIViewController
 
 @end
 
+NS_ASSUME_NONNULL_END
---
+++ RPN-Calculator-ObjC/View Controller/RPNCalculatorViewController.m
@@ -0,0 +1,58 @@
+//
+//  RPNCalculatorViewController.m
+//  RPN-Calculator-ObjC
+//
+//  Created by Iyin Raphael on 3/7/19.
+//  Copyright © 2019 Iyin Raphael. All rights reserved.
+//
+
+#import "RPNCalculatorViewController.h"
+
+@interface RPNCalculatorViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *textView;
+
+
+- (IBAction)numberButtonTapped:(id)sender;
+- (IBAction)decimalButtonTapped:(id)sender;
+- (IBAction)returnButtonTapped:(id)sender;
+- (IBAction)addButtonTapped:(id)sender;
+- (IBAction)subtractButtonTapped:(id)sender;
+- (IBAction)divideButtonTapped:(id)sender;
+- (IBAction)multiplyButtonTapped:(id)sender;
+
+
+@end
+
+@implementation RPNCalculatorViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+
+
+- (IBAction)numberButtonTapped:(id)sender {
+    
+}
+
+
+- (IBAction)decimalButtonTapped:(id)sender {
+}
+
+- (IBAction)returnButtonTapped:(id)sender {
+}
+
+- (IBAction)addButtonTapped:(id)sender {
+}
+
+- (IBAction)subtractButtonTapped:(id)sender {
+}
+
+- (IBAction)divideButtonTapped:(id)sender {
+}
+
+- (IBAction)multiplyButtonTapped:(id)sender {
+}
+@end
