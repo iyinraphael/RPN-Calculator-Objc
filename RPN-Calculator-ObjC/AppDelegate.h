e2915acd54c9c7a237db5a1babe8715e1b0f5e22
iyinraphael <iyinraphael@gmail.com>
2019-03-07 12:57:54 -0800

    initial commit

---
+++ RPN-Calculator-ObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RPN-Calculator-ObjC
+//
+//  Created by Iyin Raphael on 3/7/19.
+//  Copyright © 2019 Iyin Raphael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
